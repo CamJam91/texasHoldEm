e4bb7df451ea94b7e3c9e9d46bf18de1ae7ea47f
CamJam <cameron.e.murphy@gmail.com>
2024-04-19 13:48:16 -0400

    incorporate endgame functions into main

--- main.cpp
+++ main.cpp
@@ -15,6 +15,16 @@ void moneyDisplay(int, int, int);
 void handReset(vector<int>&, vector<char>&, int);
 bool opponentBet(int&, int&);
 bool bet(int&, int&, int&);
+int highCard(vector<int>);
+int pairs(vector<int>);
+bool threeKind(vector<int>);
+bool fourKind(vector<int>);
+bool flush(vector<char>);
+void highCheck(vector<int>, vector<int>, int, int&);
+void pairCheck(vector<int>, vector<int>, int, int&);
+void threeCheck(vector<int>, vector<int>, int, int&);
+void fourCheck(vector<int>, vector<int>, int, int&);
+void flushCheck(vector<char>, vector<char>, vector<int>, vector<int>, int, int&);
 
 
 
@@ -129,9 +139,11 @@ int main(){
                    popNumber = 7; //used to reset hand vector 
                     }
                     if (!fold){ //now hands are checked for winner  
+                        popNumber = 7;
                         handDisplay(playerHandNum, playerHandSuit,7);
                         cout << "Opponent ";
                         handDisplay(opponentHandNum, opponentHandSuit,2); //Show 2 opponent cards 
+                        
                     }
                 }
             }
@@ -141,28 +153,11 @@ int main(){
     }while(userOption != 2);
 }
 
-int randomGen(){
-    return (rand() % 52);
-    //random number generator will give us numbers from 0 -51 for use in pullinng cards from deck using draw function
-}
 
-void draw(int (&cardNum)[52], char (&cardSuit)[52], vector<int>&handNum, vector<char>&handSuit){ //2 vectors are referenced: the number of the card and the suit, an int will hold number, and char will hold suit
-    int randomNum = randomGen();    //Weput the random number into a variable so we can use it in multiple instances
-    int pullNum = 0;
-    pullNum = cardNum[randomNum];   //our random number decides which card is pulled
-    while(pullNum == 0){   //while loop checks is card = 0, if it does ir has been pulled and we run our generator again
-        randomNum = randomGen();
-        pullNum = cardNum[randomNum];
-    }
-    handNum.push_back(pullNum); //since our arrays are parallel our suit is decided by which array index is pulled
-    handSuit.push_back(cardSuit[randomNum]);        //since number and suit are parallel arrays we use the same number for suit and no need to erase the value in the actual array, if we pull from our num array correctly this will always be correct
-    cardNum[randomNum] = 0;             //We set the card that was pulled to 0 to ensure it isn't pulled again
-        /*This function uses the randomGen function to create a number 0-51 and use that numbe to pull a cad from the deck.
-        The index in the array that was pulled from is then replaced with a zero. This ensures that every card is unique. 
-        the randomGen may and will give us numbers that have already been pulled from the array which is why we only push
-        the cards when our card pulled != zero.*/
-}
 
+    /*****************************************************************************
+     **************Menu and Display functions*************************************
+    ******************************************************************************/
 int menu(int money){
     int userOption;
     cout << "Money: $" << money << endl;
@@ -170,19 +165,6 @@ int menu(int money){
     cin >> userOption;
     return userOption;
     //A simple function to kick off the gameplay loop
-}
-
-void handMimic(vector<int>playerNum, vector<char>playerSuit, vector<int>&opponentNum, vector<char>&opponentSuit, int missing){//The first 2 vectors are read only but the second are changed so we pass a reference to vector address
-    //since flop and street are community cards they need to be copied into opponent hand
-    //the missing parameter is where we pass the subscript of the community cards that we need to copy
-    int numFill;
-    char suitFill;   //interum variables to hold hand values
-    numFill = playerNum[missing-1]; 
-    suitFill = playerSuit[missing-1];
-    opponentNum.push_back(numFill);
-    opponentSuit.push_back(suitFill);   //push values to opponent vector
-    //This function takes in the parallel player hand arrays and the parallel opponent hand arrays. It also takes the index that needs to be copied. 
-    //Because it works with one index at a time it needs to be called for every card that needs copied.  
 }
 
 void handDisplay(vector<int>handNum, vector<char> handSuit, int handSize){
@@ -236,6 +218,50 @@ void moneyDisplay(int pool, int stake, int money){
 
 }
 
+
+
+
+    /*****************************************************************************
+     **************Deck and hand functions****************************************
+    ******************************************************************************/
+
+int randomGen(){
+    return (rand() % 52);
+    //random number generator will give us numbers from 0 -51 for use in pullinng cards from deck using draw function
+}
+
+void draw(int (&cardNum)[52], char (&cardSuit)[52], vector<int>&handNum, vector<char>&handSuit){ //2 vectors are referenced: the number of the card and the suit, an int will hold number, and char will hold suit
+    int randomNum = randomGen();    //Weput the random number into a variable so we can use it in multiple instances
+    int pullNum = 0;
+    pullNum = cardNum[randomNum];   //our random number decides which card is pulled
+    while(pullNum == 0){   //while loop checks is card = 0, if it does ir has been pulled and we run our generator again
+        randomNum = randomGen();
+        pullNum = cardNum[randomNum];
+    }
+    handNum.push_back(pullNum); //since our arrays are parallel our suit is decided by which array index is pulled
+    handSuit.push_back(cardSuit[randomNum]);        //since number and suit are parallel arrays we use the same number for suit and no need to erase the value in the actual array, if we pull from our num array correctly this will always be correct
+    cardNum[randomNum] = 0;             //We set the card that was pulled to 0 to ensure it isn't pulled again
+        /*This function uses the randomGen function to create a number 0-51 and use that numbe to pull a cad from the deck.
+        The index in the array that was pulled from is then replaced with a zero. This ensures that every card is unique. 
+        the randomGen may and will give us numbers that have already been pulled from the array which is why we only push
+        the cards when our card pulled != zero.*/
+}
+
+
+void handMimic(vector<int>playerNum, vector<char>playerSuit, vector<int>&opponentNum, vector<char>&opponentSuit, int missing){//The first 2 vectors are read only but the second are changed so we pass a reference to vector address
+    //since flop and street are community cards they need to be copied into opponent hand
+    //the missing parameter is where we pass the subscript of the community cards that we need to copy
+    int numFill;
+    char suitFill;   //interum variables to hold hand values
+    numFill = playerNum[missing-1]; 
+    suitFill = playerSuit[missing-1];
+    opponentNum.push_back(numFill);
+    opponentSuit.push_back(suitFill);   //push values to opponent vector
+    //This function takes in the parallel player hand arrays and the parallel opponent hand arrays. It also takes the index that needs to be copied. 
+    //Because it works with one index at a time it needs to be called for every card that needs copied.  
+}
+
+
 void handReset(vector<int>& handNum, vector<char>& handSuit, int popNum){
     for (int counter = 0; counter < popNum; counter++){
         handNum.pop_back();
@@ -245,6 +271,13 @@ void handReset(vector<int>& handNum, vector<char>& handSuit, int popNum){
     every time. To resetthe hand vectors we pop back by the number of cards that were dealt.*/
 }
 
+
+
+
+
+/*****************************************************************************
+     **************Bet and AI functions****************************************
+    ******************************************************************************/
 bool opponentBet(int& stake, int& pool){
     cout << "opponent checks" << endl;
     stake+=stake;
@@ -295,4 +328,170 @@ bool bet(int& stake, int& money,int& pool){ //pass stake as a reference so we ca
     /*This function works like a void function in that most of what it does is displaying something and affecting variables passed to it. It will
     always reutrn false(which will do nothing in main) unless there is a fold. It is meant to be run in a do while loop. When there is a raise/bet it affects
     stake. Which will keep the loop going. if a check or call is made stake is 0 so the loop stops. */
+}
+
+
+
+
+/*****************************************************************************
+     **************Game end/Win check functions********************************
+    ******************************************************************************/
+       
+        /********Hand Algorithms*************/
+int highCard(vector<int> handNum){
+    int highCard = 0;
+    for (int card = 0; card < 7; card++){
+        if (highCard != 1 && handNum[card] > highCard || handNum[card] == 1){
+            //this if condition is clunky but its easier for other algorithms if ace is represented by 1
+            //but we still have ot make it high
+            highCard = handNum[card];
+        }
+    }
+    return highCard;
+        //Function to find the highest number except in this case 1 will resolve as the highest because it represents the Ace
+} 
+
+int pairs(vector<int> handNum){
+    int numberofPairs = 0;
+    for (int card = 0; card < 6; card++){ //we dont need to check the lat card it will already be checked on next to last
+        for (int nextCard = card + 1; nextCard < 7; nextCard++){ //keeps card to check the same but loops through every other one
+            if (handNum[card] == handNum[nextCard])
+                numberofPairs++;
+        }
+    }
+    return numberofPairs;
+}
+
+bool threeKind(vector<int> handNum){
+    bool threeKind = false;
+    for (int card = 0; card <= 5 && threeKind == false; card++){ //loop stops at card 5 bc a three match would be impossible
+        int matches = 0; //if this == 2 the we have found 3 cards of the same number, careful of 1 off error
+        for (int nextCard = card + 1; nextCard < 7; nextCard++){    //compare all 7 cards to target card
+            if (handNum[card] == handNum[nextCard])
+                matches++;
+        }
+        if (matches == 2)
+            threeKind = true; //after each card is checked against every other we see if we have 3 of a kind
+    }
+    return threeKind;
+        //The first for loop is for picking every card(except last 2) to compare, the nested is for looping through every
+        //other card and comparing it to the first. Matches is defined inside outer loop so it resets before actual card
+        //comparison. if a match is found we must change our bool terminator before another loop starts
+}
+
+bool fourKind(vector<int> handNum){
+    bool fourKind = false;
+    for (int card = 0; card <= 4 && fourKind == false; card++){ //loop stops at card 4 bc a three match would be impossible
+        int matches = 0; //if this == 3 the we have found 4 cards of the same number, careful of 1 off error
+        for (int nextCard = card + 1; nextCard < 7; nextCard++){    //compare all 7 cards to target card
+            if (handNum[card] == handNum[nextCard])
+                matches++;
+        }
+        if (matches == 3)
+            fourKind = true; //after each card is checked against every other we see if we have 3 of a kind
+    }
+    return fourKind;
+        //The first for loop is for picking every card(except last 2) to compare, the nested is for looping through every
+        //other card and comparing it to the first. Matches is defined inside outer loop so it resets before actual card
+        //comparison. if a match is found we must change our bool terminator before another loop starts
+}
+
+bool flush(vector<char>handSuit){
+    bool flush = false;
+    for (int card = 0; card < 3 && flush == false; card++){   //if a flush isnt found after card 3, flush is impossible
+        int match = 0;
+        for (int nextCard = card + 1; nextCard < 7; nextCard++){
+            if (handSuit[card] == handSuit[nextCard])
+                match++;
+        }
+        if (match >= 4)
+            flush = true;
+    }
+    return flush;
+}
+
+
+        /********Hand Checks for main*************/
+
+void highCheck(vector<int> playerHandNum, vector<int>opponentHandNum, int pool, int& playerMoney){ //playerMoney needs to be passed as a reference so we can add to it
+    int playerHigh = highCard(playerHandNum);
+    int opponentHigh = highCard(opponentHandNum);//return highest value card
+    
+    if (playerHigh > opponentHigh){
+        cout << "You win with a High Card!" << endl << "Winnings: $"
+        << pool << endl;
+        playerMoney+=pool;
+        
+    }else if(opponentHigh > playerHigh){
+        cout << "You lose." << endl << "Pool: $" << pool <<endl;
+    }else{
+        cout << "Draw" << endl << "Split: $" << pool/2 << endl;
+        playerMoney+=(pool/2);
+    }
+    //This function uses the highCard function to check for a winner. There are three outcomes, a win
+    //Wherein the player will be rewarded the pool, a draw where they are rewarded half the pool, and a
+    //lose. Where nothing happens, meaning pool is lost. 
+}
+
+void pairCheck(vector<int>playerHandNum, vector<int>opponentHandNum, int pool, int& playerMoney){
+    int playerPair = pairs(playerHandNum);
+    int opponentPair = pairs(opponentHandNum);
+
+    if(playerPair > opponentPair){
+        cout << "You win with " << playerPair << " pair" << endl << "Winnings: $"
+        << pool << endl;
+        playerMoney+=pool;
+    }else if(opponentPair > playerPair){
+        cout << "You lose." << endl << "Pool: $" << pool <<endl;
+    }else if(playerPair == opponentPair){
+        highCheck(playerHandNum, opponentHandNum, pool, playerMoney);
+    }
+}
+
+void threeCheck(vector<int>playerHandNum, vector<int>opponentHandNum, int pool, int& playerMoney){
+    bool playerthreeKind = threeKind(playerHandNum);
+    bool opponentthreeKind = threeKind(opponentHandNum);
+
+    if(playerthreeKind && !opponentthreeKind){
+        cout << "You win with a Three of a Kind!" << endl << "Winnings: $"
+        << pool << endl;
+        playerMoney+=pool;
+    }else if(opponentthreeKind && !playerthreeKind){
+        cout << "You lose." << endl << "Pool: $" << pool <<endl;
+    }else if(playerthreeKind && opponentthreeKind){
+        highCheck(playerHandNum, opponentHandNum, pool, playerMoney);
+    }
+}
+
+void fourCheck(vector<int>playerHandNum, vector<int>opponentHandNum, int pool, int& playerMoney){
+    bool playerfourKind = fourKind(playerHandNum);
+    bool opponentfourKind = fourKind(opponentHandNum);
+
+    if(playerfourKind && !opponentfourKind){
+        cout << "You win with a Four of a Kind!" << endl << "Winnings: $"
+        << pool << endl;
+        playerMoney+=pool;
+    }else if(opponentfourKind && !playerfourKind){
+        cout << "You lose." << endl << "Pool: $" << pool <<endl;
+    }else if(playerfourKind && opponentfourKind){
+        highCheck(playerHandNum, opponentHandNum, pool, playerMoney);
+    }
+}
+
+void flushCheck(vector<char>playerHand, vector<char>opponentHand,vector<int>playerHandNum, vector<int>opponentHandNum, int pool, int& playerMoney){
+    bool playerFlush = flush(playerHand);
+    bool opponentFlush = flush(opponentHand);
+
+    if(playerFlush && !opponentFlush){
+        cout << "You win with a Flush!" << endl << "Winnings: $"
+        << pool << endl;
+        playerMoney+=pool;
+    }else if(opponentFlush && !playerFlush){
+        cout << "You lose." << endl << "Pool: $" << pool <<endl;
+    }else if(playerFlush && opponentFlush){
+        highCheck(playerHandNum, opponentHandNum, pool, playerMoney);
+    }
+    //This function calls the flush function for both hands and will trigger a win or loss if either hand has an exlusive flush
+    //If both have a flush, it calls highCard and decides the winner based on that, which is why the handNum vectors are passed.
+    //If neither have a flush it should do nothing. 
 }
